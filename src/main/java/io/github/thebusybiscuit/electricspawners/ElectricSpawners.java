6fa0de2aab171ba4715455390587c4f539f696df
ybw0014 <i@guizhanss.com>
2023-08-10 16:21:58 -0400

    chore: bump dependencies

--- src/main/java/io/github/thebusybiscuit/electricspawners/ElectricSpawners.java
+++ src/main/java/io/github/thebusybiscuit/electricspawners/ElectricSpawners.java
@@ -7,7 +7,7 @@ import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.skins.PlayerHead;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.skins.PlayerSkin;
-import net.guizhanss.guizhanlib.updater.GuizhanBuildsUpdater;
+import net.guizhanss.guizhanlibplugin.updater.GuizhanUpdater;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.EntityType;
@@ -20,14 +20,20 @@ public class ElectricSpawners extends JavaPlugin implements Listener, SlimefunAd
 
     @Override
     public void onEnable() {
+        if (!getServer().getPluginManager().isPluginEnabled("GuizhanLibPlugin")) {
+            getLogger().log(Level.SEVERE, "本插件需要 鬼斩前置库插件(GuizhanLibPlugin) 才能运行!");
+            getLogger().log(Level.SEVERE, "从此处下载: https://50L.cc/gzlib");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         Config cfg = new Config(this);
 
         // Setting up bStats
         new Metrics(this, 6163);
 
-        if (cfg.getBoolean("options.auto-update") &&
-            getDescription().getVersion().startsWith("Build ")) {
-            new GuizhanBuildsUpdater(this, getFile(), "ybw0014", "ElectricSpawners-CN", "master", false).start();
+        if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("Build")) {
+            GuizhanUpdater.start(this, getFile(), "SlimefunGuguProject", "ElectricSpawners", "master");
         }
 
         ItemGroup itemGroup = new ItemGroup(new NamespacedKey(this, "electric_spawners"), new CustomItemStack(PlayerHead.getItemStack(PlayerSkin.fromHashCode("db6bd9727abb55d5415265789d4f2984781a343c68dcaf57f554a5e9aa1cd")), "&9电力刷怪笼"));
@@ -54,6 +60,6 @@ public class ElectricSpawners extends JavaPlugin implements Listener, SlimefunAd
 
     @Override
     public String getBugTrackerURL() {
-        return "https://github.com/ybw0014/ElectricSpawners-CN/issues";
+        return "https://github.com/SlimefunGuguProject/ElectricSpawners/issues";
     }
 }
